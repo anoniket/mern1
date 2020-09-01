b4c8a915c933d58f1b17372f0bc0e834691ed02d
aniket <aniket.kh18@gmail.com>
2020-09-01 18:03:12 +0530

    third deploy try

---
+++ client/build/precache-manifest.cd49499f70e7f6ffa175a2b0872d4b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cf5a0b5155196e88f211760dc20394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a58319b937cad9af8809",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a58319b937cad9af8809",
+    "url": "/static/js/2.40fc4e4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.40fc4e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e46bc82f5d97af2631",
+    "url": "/static/js/main.9ea0d1ef.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
